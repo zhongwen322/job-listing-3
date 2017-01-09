8caacdf66f05f9f69456bb5647b74cf5ea261706
evey <zhongwen322@gmail.com>
2017-01-09 22:51:53 +1300

    implement part4

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,7 @@ class JobsController < ApplicationController
   before_action :authenticate_user!, only:[:new]
   before_filter :authenticate_user!, only:[:new, :create, :edit, :update, :destroy]
   def index
-    @jobs = Job.all
+    @jobs = Job.where(:is_hidden => false).order("created_at DESC")
   end
 
   def new
