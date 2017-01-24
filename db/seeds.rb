88d12208fcbbe2a2b4d52d3ad1e0d2d92f1a2e1f
evey <zhongwen322@gmail.com>
2017-01-25 10:59:24 +1300

    create admin and user account by using seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-create_account = User.create([email: 'zhongwen322@gmail.com', password:'wyyzzl813', password_confirmation: 'wyyzzl813', is_admin: 'true'])
+puts "這個種子檔會自動建立一個admin帳號, 並且創建 10 個 public jobs, 以及10個hidden jobs"
+
+create_account = User.create([email: 'zhongwen322@gmail.com', password: 'wyyzzl813', password_confirmation: 'zhongwen322@gmail.com', is_admin: 'true'])
+puts "Admin account created."
+
+create_account_user = User.create([email: "b@b.com", password:"123456",password_confirmation: "123456",is_admin:"false"])
+
+puts "User account created."
+
+
+create_jobs = for i in 1..10 do
+  Job.create!([title: "Job no.#{i}", description: "這是用種子建立的第 #{i} 個Public工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100, is_hidden: "false"])
+end
+puts "10 Public jobs created."
+
+create_jobs = for i in 1..10 do
+  Job.create!([title: "Job no.#{i+10}", description: "這是用種子建立的第 #{i+10} 個Hidden工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100,is_hidden: "true"])
+end
+puts "10 Hidden jobs created."
